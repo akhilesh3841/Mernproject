1493eacc00b0620f9d2c86a3845b673d3e345fe7
Akhilesh Yadav <akhileshyadav3841@gmail.com>
2025-05-24 23:19:55 +0530

    lastcommit

--- frontend/src/utils/helper.jsx
+++ frontend/src/utils/helper.jsx
@@ -2,7 +2,7 @@
 // export const Base_url=location.hostname === "localhost"?"http://localhost:3000":"/api"
 
 // export const Base_url="https://mernproject-rho.vercel.app/"
-export const Base_url="https://mernproject-gold.vercel.app/"
+export const Base_url="http://localhost:3000"
 
 // export const Base_url="/api";
 
